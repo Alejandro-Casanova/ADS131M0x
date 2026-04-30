cce5a9f8ecbc137d99dd04657f01e0e51c10863f
Alejandro Casanova <alejandro.casanova@disroot.org>
2026-04-30 16:22:40 +0200

    chore: clean up macro definitions

--- src/ADS131M0x.h
+++ src/ADS131M0x.h
@@ -21,46 +21,61 @@ struct adcOutput
 #endif
 };
 
-#define DRDY_STATE_LOGIC_HIGH 0 // DEFAULT
+#define DRDY_STATE_LOGIC_HIGH 0  // DEFAULT
 #define DRDY_STATE_HI_Z 1
 
 #define POWER_MODE_VERY_LOW_POWER 0
 #define POWER_MODE_LOW_POWER 1
-#define POWER_MODE_HIGH_RESOLUTION 2 // DEFAULT
+#define POWER_MODE_HIGH_RESOLUTION 2  // DEFAULT
 
-#define CHANNEL_PGA_1 0
-#define CHANNEL_PGA_2 1
-#define CHANNEL_PGA_4 2
-#define CHANNEL_PGA_8 3
-#define CHANNEL_PGA_16 4
-#define CHANNEL_PGA_32 5
-#define CHANNEL_PGA_64 6
-#define CHANNEL_PGA_128 7
-
-#define INPUT_CHANNEL_MUX_AIN0P_AIN0N 0 // DEFAULT
+#define INPUT_CHANNEL_MUX_AIN0P_AIN0N 0  // DEFAULT
 #define INPUT_CHANNEL_MUX_INPUT_SHORTED 1
 #define INPUT_CHANNEL_MUX_POSITIVE_DC_TEST_SIGNAL 2
 #define INPUT_CHANNEL_MUX_NEGATIVE_DC_TEST_SIGNAL 3
 
+// Clock Type
+#define CLOCK_EXTERNAL 1
+#define CLOCK_INTERNAL 0
+
+// PGA Gain
+#define PGA_GAIN_1 0
+#define PGA_GAIN_2 1
+#define PGA_GAIN_4 2
+#define PGA_GAIN_8 3
+#define PGA_GAIN_16 4
+#define PGA_GAIN_32 5
+#define PGA_GAIN_64 6
+#define PGA_GAIN_128 7
+
 #define OSR_128 0
 #define OSR_256 1
 #define OSR_512 2
-#define OSR_1024 3      // DEFAULT
+#define OSR_1024 3  // DEFAULT
 #define OSR_2018 4
 #define OSR_4096 5
 #define OSR_8192 6
 #define OSR_16384 7
 
+// ----------------------------------------------------------------------------
 
 // Commands
-#define CMD_NULL 0x0000 // This command gives the STATUS REGISTER
+#define CMD_NULL 0x0000  // This command gives the STATUS REGISTER
 #define CMD_RESET 0x0011
 #define CMD_STANDBY 0x0022
 #define CMD_WAKEUP 0x0033
 #define CMD_LOCK 0x0555
 #define CMD_UNLOCK 0x0655
-#define CMD_READ_REG 0xA000 // 101a aaaa annn nnnn   a=address  n=num  regis-1
+#define CMD_READ_REG 0xA000
 #define CMD_WRITE_REG 0x6000
+
+// Masks for commands RREG and WREG
+// They follow structure "101a aaaa annn nnnn" /* cSpell:disable-line */
+// Where "a" is the address of the register and "n" is the number of bytes to
+// read/write - 1 (e.g. 0 for 1 byte, 1 for 2 bytes, etc.)
+// In the write response, "n" is the number of bytes actually written,
+// which may be different from the number of bytes requested in the command
+#define CMDMASK_RWREG_ADDRESS 0x1F80
+#define CMDMASK_RWREG_NBYTES 0x007F
 
 // Responses
 #ifdef IS_M02
@@ -69,6 +84,8 @@ struct adcOutput
 #define RSP_RESET_OK 0xFF24
 #endif
 #define RSP_RESET_NOK 0x0011
+
+// ----------------------------------------------------------------------------
 
 // Registers Read Only
 #define REG_ID 0x00
@@ -113,13 +130,12 @@ struct adcOutput
 // Registers MAP CRC
 #define REG_MAP_CRC 0x3E
 
-// ------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 
-// Mask READ_REG
-#define REGMASK_CMD_READ_REG_ADDRESS 0x1F80
-#define REGMASK_CMD_READ_REG_BYTES 0x007F
+// Masks for Register ID
+#define REGMASK_ID_CHANCNT 0x0F00
 
-// Mask Register STATUS
+// Masks for Register STATUS
 #define REGMASK_STATUS_LOCK 0x8000
 #define REGMASK_STATUS_RESYNC 0x4000
 #define REGMASK_STATUS_REGMAP 0x2000
@@ -132,7 +148,7 @@ struct adcOutput
 #define REGMASK_STATUS_DRDY1 0x0002
 #define REGMASK_STATUS_DRDY0 0x0001
 
-// Mask Register MODE
+// Masks for Register MODE
 #define REGMASK_MODE_REG_CRC_EN 0x2000
 #define REGMASK_MODE_RX_CRC_EN 0x1000
 #define REGMASK_MODE_CRC_TYPE 0x0800
@@ -143,7 +159,7 @@ struct adcOutput
 #define REGMASK_MODE_DRDY_HiZ 0x0002
 #define REGMASK_MODE_DRDY_FMT 0x0001
 
-// Mask Register CLOCK
+// Masks for Register CLOCK
 #define REGMASK_CLOCK_CH3_EN 0x0800
 #define REGMASK_CLOCK_CH2_EN 0x0400
 #define REGMASK_CLOCK_CH1_EN 0x0200
@@ -151,13 +167,13 @@ struct adcOutput
 #define REGMASK_CLOCK_OSR 0x001C
 #define REGMASK_CLOCK_PWR 0x0003
 
-// Mask Register GAIN
+// Masks for Register GAIN
 #define REGMASK_GAIN_PGAGAIN3 0x7000
 #define REGMASK_GAIN_PGAGAIN2 0x0700
 #define REGMASK_GAIN_PGAGAIN1 0x0070
 #define REGMASK_GAIN_PGAGAIN0 0x0007
 
-// Mask Register CFG
+// Masks for Register CFG
 #define REGMASK_CFG_GC_DLY 0x1E00
 #define REGMASK_CFG_GC_EN 0x0100
 #define REGMASK_CFG_CD_ALLCH 0x0080
@@ -165,93 +181,24 @@ struct adcOutput
 #define REGMASK_CFG_CD_LEN 0x000E
 #define REGMASK_CFG_CD_EN 0x0001
 
-// Mask Register THRSHLD_LSB
+// Masks for Register THRSHLD_LSB
 #define REGMASK_THRSHLD_LSB_CD_TH_LSB 0xFF00
 #define REGMASK_THRSHLD_LSB_DCBLOCK 0x000F
 
-// Mask Register CHX_CFG
+// Masks for Register CHX_CFG
 #define REGMASK_CHX_CFG_PHASE 0xFFC0
 #define REGMASK_CHX_CFG_DCBLKX_DIS0 0x0004
 #define REGMASK_CHX_CFG_MUX 0x0003
 
-// Mask Register CHX_OCAL_LSB
+// Masks for Register CHX_OCAL_LSB
 #define REGMASK_CHX_OCAL0_LSB 0xFF00
 
-// Mask Register CHX_GCAL_LSB
+// Masks for Register CHX_GCAL_LSB
 #define REGMASK_CHX_GCAL0_LSB 0xFF00
 
-//   --------------------------------------------------------------------
-
-// Conversion modes
-#define CONVERSION_MODE_CONT 0
-#define CONVERSION_MODE_SINGLE 1
-
-// Data Format
-#define DATA_FORMAT_TWO_COMPLEMENT 0
-#define DATA_FORMAT_BINARY 1
-
-// Measure Mode
-#define MEASURE_UNIPOLAR 1
-#define MEASURE_BIPOLAR 0
-
-// Clock Type
-#define CLOCK_EXTERNAL 1
-#define CLOCK_INTERNAL 0
-
-// PGA Gain
-#define PGA_GAIN_1 0
-#define PGA_GAIN_2 1
-#define PGA_GAIN_4 2
-#define PGA_GAIN_8 3
-#define PGA_GAIN_16 4
-#define PGA_GAIN_32 5
-#define PGA_GAIN_64 6
-#define PGA_GAIN_128 7
-
-// Input Filter
-#define FILTER_SINC 0
-#define FILTER_FIR 2
-#define FILTER_FIR_IIR 3
-
-// Data Mode
-#define DATA_MODE_24BITS 0
-#define DATA_MODE_32BITS 1
-
-// Data Rate
-#define DATA_RATE_0 0
-#define DATA_RATE_1 1
-#define DATA_RATE_2 2
-#define DATA_RATE_3 3
-#define DATA_RATE_4 4
-#define DATA_RATE_5 5
-#define DATA_RATE_6 6
-#define DATA_RATE_7 7
-#define DATA_RATE_8 8
-#define DATA_RATE_9 9
-#define DATA_RATE_10 10
-#define DATA_RATE_11 11
-#define DATA_RATE_12 12
-#define DATA_RATE_13 13
-#define DATA_RATE_14 14
-#define DATA_RATE_15 15
-
-// Sync Mpdes
-#define SYNC_CONTINUOUS 1
-#define SYNC_PULSE 0
-
-// DIO Config Mode
-#define DIO_OUTPUT 1
-#define DIO_INPUT 0
-
-#define SPI_MASTER_DUMMY 0xFF
-#define SPI_MASTER_DUMMY16 0xFFFF
-#define SPI_MASTER_DUMMY32 0xFFFFFFFF
-
-// by JG: not tested !!
-#define MACROVALCALC(val,offset, factor, divisor) (((val - offset) * factor / divisor))
-
 /**
- * @brief Arduino class for the TI ADS131M02 and ADS131M04 ADC-converter with SPI Interface
+ * @brief Arduino class for the TI ADS131M02 and ADS131M04 ADC-converter with 
+ * SPI Interface
  * 
  */
 class ADS131M0x
