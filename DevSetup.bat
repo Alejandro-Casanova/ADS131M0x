6a24784a49eeaa80da462a995a36073ee2842a93
Alejandro Casanova <alejandro.casanova@disroot.org>
2026-04-30 16:12:53 +0200

    chore: add development tools

---
+++ DevSetup.bat
@@ -0,0 +1,8 @@
+ECHO OFF
+
+REM This batch file is used to set up the development environment for the ADS131M0x library.
+REM It installs the necessary dependencies and configures the project settings.
+
+REM Install clang-format among other npm dependencies
+ECHO Installing node_modules...
+npm i
